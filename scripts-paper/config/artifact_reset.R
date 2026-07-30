93641d9bfc34a0ed04022623b98591dd487dbfdf
Fernando Duarte <duarte@alum.mit.edu>
2026-07-29 22:43:09 -0400

    Add the six named cleanup compartments and the reset orchestrator

--- scripts-paper/config/artifact_reset.R
+++ scripts-paper/config/artifact_reset.R
@@ -35,3 +35,47 @@ artifact_records_by_group <- function(group) {
   }
   rows$id
 }
+
+cleanup_bootstrap_cache <- function() {
+  cleanup_artifacts_by_ids("bootstrap_stage_draws")
+}
+
+cleanup_gate_state <- function() {
+  cleanup_artifacts_by_ids(c(
+    "dynamics_gate", "egarch_status",
+    "conditional_route_status", "egarch_pilot_state"
+  ))
+}
+
+cleanup_diagnostics <- function() {
+  cleanup_artifacts_by_ids(artifact_records_by_group("diagnostics")$id)
+}
+
+cleanup_tables <- function(include_tracked = TRUE) {
+  cleanup_artifacts_by_ids(.reset_group_ids(
+    c("tables", "tables/descriptive_statistics"), include_tracked
+  ))
+}
+
+cleanup_figures <- function(include_tracked = TRUE) {
+  cleanup_artifacts_by_ids(.reset_group_ids(
+    c("figures", "figures/descriptive_statistics"), include_tracked
+  ))
+}
+
+cleanup_reports <- function(include_tracked = TRUE) {
+  cleanup_artifacts_by_ids(.reset_group_ids("reports", include_tracked))
+}
+
+reset_pipeline_state <- function(include_tracked = TRUE) {
+  audits <- list(
+    bootstrap_cache = cleanup_bootstrap_cache(),
+    gate_state = cleanup_gate_state(),
+    diagnostics = cleanup_diagnostics(),
+    tables = cleanup_tables(include_tracked),
+    figures = cleanup_figures(include_tracked),
+    reports = cleanup_reports(include_tracked)
+  )
+  audits$latex_sidecars <- clean_latex_sidecars(out_dir)
+  audits
+}
